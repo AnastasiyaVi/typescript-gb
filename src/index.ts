55ad9563b8ad5ebd979bd5f69b4b7b8fcc84a838
AnastasiyaVi <nastyonaki@mail.ru>
2023-01-19 10:59:10 +0300

    homework for lesson 3

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,30 @@ import { renderUserBlock } from './user.js'
 import { renderToast } from './lib.js'
 import { getUserData } from './getUserData.js'
 import { getFavoritesAmount } from "./getFavoritesAmount.js";
+import { getCollection, getTodosByCount } from "./getCollection.js";
+
+
+getCollection("users", 1).catch((error?: string) => {
+  throw error
+});
+getCollection("todos", 1).catch((error?: string) => {
+  throw error
+});
+getCollection("posts", 1).catch((error?: string) => {
+  throw error
+});
+getCollection("comments", 1).catch((error?: string) => {
+  throw error
+});
+getCollection("photos", 2).catch((error?: string) => {
+  throw error
+});
+getCollection("albums", 1).catch((error?: string) => {
+  throw error
+});
+getTodosByCount(7).catch((error?: string) => {
+  throw error
+});
 
 window.addEventListener('DOMContentLoaded', () => {
   const userData = getUserData()
