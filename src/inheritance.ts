51d6d18d4a50f9696f2704b7c1acf86be9bcf4cc
AnastasiyaVi <nastyonaki@mail.ru>
2023-01-30 09:33:24 +0300

    homework for lesson 6

--- src/inheritance.ts
+++ src/inheritance.ts
@@ -4,17 +4,10 @@ export interface Center {
 }
 
 export abstract class MyGraphicsPrimitive2D {
-  protected top: number;
-  protected bottom: number;
-  protected left: number;
-  protected right: number;
-
-  constructor(top?: number, left?: number, bottom?: number, right?: number) {
-    this.left = left;
-    this.top = top;
-    this.bottom = bottom;
-    this.right = right;
-  }
+  protected top: number = 0;
+  protected bottom: number = 0;
+  protected left: number = 0;
+  protected right: number = 0;
 
   moveVertical(moving: number): number {
     this.top = this.top + moving;
@@ -72,24 +65,19 @@ export class MyRectangle extends MyAreaPrimitive2D {
   public right: number;
 
   constructor(top: number, left: number, bottom: number, right: number) {
-    super(top, left, bottom, right);
+    super();
+    this.left = left;
+    this.top = top;
+    this.bottom = bottom;
+    this.right = right;
   }
 
-  width(): number | null {
-    if (this.right > this.left) {
-      return this.right - this.left;
-    } else {
-      return null;
-    }
+  width(): number {
+    return this.right - this.left;
   }
 
-  height(): number | null {
-    if (this.bottom > this.top) {
-      return this.bottom - this.top;
-    } else {
-      return null;
-    }
-
+  height(): number {
+    return this.bottom - this.top;
   }
 
   square(): number {
